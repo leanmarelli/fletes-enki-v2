9244c18a261e8a23de0b46918e786787352baa22
leanmarelli <marellilean@gmail.com>
2025-10-29 22:19:33 -0300

    ricardo

--- js/email-sender.js
+++ js/email-sender.js
@@ -3,6 +3,7 @@ const SEND_EMAIL_URL = 'https://fletes-enki.netlify.app/.netlify/functions/send-
 const MAIL_TOKEN_PUBLIC = '8d6c2a5f0c2a4c86b5b0a9b3c3f1e7d49e2f6c1a0f8b7c4d2e9a1b3c5d7e9f0';
 
 export async function enviarEmailViaje(viaje, fletero, tipo = 'nuevo') {
+    console.log('Enviando email...', { viaje, fletero, tipo })  
     const res = await fetch(SEND_EMAIL_URL, {
         method: 'POST',
         headers: {
